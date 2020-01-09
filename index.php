69c9593f68d897cc344ed3bc97d522d51bbc07c3
MATVEN <mattiavena2993@gmail.com>
2020-01-09 19:42:15 +0100

    add residence address objects

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+/* TODO TODAY:
+
+       - Definire la classe Persona caratterizzata da nome e cognome;
+       - Definire la classe Ospite (che eredita da Persona) caratterizzata da nome, cognome e anno di nascita;
+       - Definire la classe Pagante (che eredita da Persona) caratterizzata da nome, cognome e indirizzo (di residenza);
+       - Per ogni classe definire costruttore e toString in maniera appropriata, eventualmente richiamando i metodi della classe padre;
+
+       - Eseguire dei test, istanziando ogni classe definita e testando la correttezza dei risultati attesi
+   */
+
+
 <?php
 
 class Person{
@@ -35,5 +46,26 @@ class Guest extends Person{
     return parent::__toString()."<br> Year of Birth :".$this-> yearOfBirth;
   }
 }
+
+class Payer extends Person{
+
+  public $residentialAddress
+  public $billingAddress
+
+  function __construct($name, $lastname, $residentialAddress, $billingAddress){
+
+    parent:: __construct($name, $lastname)
+
+      $this -> residentialAddress = $residentialAddress;
+      $this -> billingAddress = $billingAddress;
+  }
+
+  function __toString(){
+
+    return parent::__toString()."<br> Residential address :".$this-> residentialAddress
+                               ."<br> Billing address :".$this-> billingAddress;
+  }
+}
+
 
 ?>
